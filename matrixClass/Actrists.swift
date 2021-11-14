297cfcc98075b3e8e257a765272feffe1a8cf4cc
Mesut <mesutalver@icloud.com>
2021-11-15 01:27:19 +0300

    first commit

--- matrixClass/Actrists.swift
+++ matrixClass/Actrists.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+enum ActristCast {
+    case Neo
+    case Trinity
+    case AgentSmith
+    case Morpheus
+}
+
+
+class Actrists {
+    
+    //property-özellikler
+    
+    var name: String
+    var age: Int
+    var job: String
+    var cast: ActristCast
+    
+    
+    //Initializer- sınıftaki özelliklerimizi kullanabilmek için bunları init etmemiz gerekiyor.
+    
+    init (nameInit: String, ageInit: Int, jobInit: String, castInit: ActristCast){
+        
+        name = nameInit
+        age = ageInit
+        job = jobInit
+        cast = castInit
+        
+    }
+    
+}
--- matrixClass/main.swift
+++ matrixClass/main.swift
@@ -7,5 +7,30 @@
 
 import Foundation
 
-print("Hello, World!")
+let keanu = Actrists(nameInit: "Keanu Reeves", ageInit: 57, jobInit: "Actor", castInit: .Neo)
 
+print(keanu.name)
+print(keanu.age)
+print(keanu.job)
+print(keanu.cast)
+
+let carrie = Actrists(nameInit: "Carrie-Anne Mos", ageInit: 54, jobInit: "Actor", castInit: .Trinity)
+
+print(carrie.name)
+print(carrie.age)
+print(carrie.job)
+print(carrie.cast)
+
+let hugo = Actrists(nameInit: "Hugo Weaving", ageInit: 61, jobInit: "Actor", castInit: .AgentSmith)
+
+print(hugo.name)
+print(hugo.age)
+print(hugo.job)
+print(hugo.cast)
+
+let laurance = Actrists(nameInit: "Laurance Fishburne", ageInit: 60, jobInit: "Actor", castInit: .Morpheus)
+
+print(laurance.name)
+print(laurance.age)
+print(laurance.job)
+print(laurance.cast)
